77e28b10280927a5fa8913ea4947c26bcbff5db7
Raycaster-x <salamahector@gmail.com>
2023-03-27 12:53:09 -0600

    this is the first commit

---
+++ StringVariable/src/StringVariable.java
@@ -0,0 +1,12 @@
+public class StringVariable {
+    public static void main(String[] args) throws Exception {
+        String greeting;
+        greeting = "Good to see you here!";
+        System.out.println(greeting);
+
+        String Ghost;
+        Ghost = "Danny Phantom";
+        Ghost = "Danny";
+        System.out.println(Ghost);
+    }
+}
